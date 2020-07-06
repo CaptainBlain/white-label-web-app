ac81e40a005c14fa29614bf655903505d29995ff
Blain Ellis <blain.ellis@voxpopme.com>
2020-07-06 15:10:43 +0100

    working

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,11 +5,13 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {HomeRoutingModule} from './home-routing.module';
 import {CommonModule} from '@angular/common';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {FirstComponent} from './first/first.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    FirstComponent
   ],
   imports: [
     RouterModule,
